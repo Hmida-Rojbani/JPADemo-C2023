2d935b2529272c498cbed0df4ee458bdc95ce04f
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-11 15:24:19 +0100

    add LocationRepository

---
+++ src/main/java/de/tekup/jpademoc/repos/LocationRepository.java
@@ -0,0 +1,7 @@
+package de.tekup.jpademoc.repos;
+
+import de.tekup.jpademoc.entites.Location;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationRepository extends CrudRepository<Location,Integer> {
+}
