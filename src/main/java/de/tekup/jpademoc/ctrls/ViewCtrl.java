71616003656ee4fffa1deb61cdccf6c0329224a0
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-11-08 14:18:44 +0100

    add template and thymelaf

---
+++ src/main/java/de/tekup/jpademoc/ctrls/ViewCtrl.java
@@ -0,0 +1,25 @@
+package de.tekup.jpademoc.ctrls;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class ViewCtrl {
+
+    @GetMapping({"/","/index"})
+    public String landingPage(Model model){
+        String today = LocalDate.now()
+                .format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+        model.addAttribute("date",today);
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+}
